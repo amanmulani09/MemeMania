f9d8e4f5c45dbac3a1b2c031fae0ec2bb8f01e5d
amanmulani09 <mulaniaman0504@gmail.com>
2023-08-13 14:05:11 +0530

    implmented fetching data

--- src/components/MemeContainer.tsx
+++ src/components/MemeContainer.tsx
@@ -3,7 +3,7 @@ import { useGetTrendingMemeQuery } from "@/features/api/meme";
 import {useState,useEffect} from 'react'
 import Meme from "./Meme";
 const MemeContainer = ()=>{
-const [fetchCategory,setFetchCategory] = useState('trending')
+const [fetchCategory,setFetchCategory] = useState('top')
 const {data,error,isLoading} = useGetTrendingMemeQuery(fetchCategory);
 
 console.log(data)
@@ -11,19 +11,19 @@ if(isLoading) return <img alt="loading" src="/loader.gif" width={200} height={20
 return(
     <>
     <aside className="rounded border border-slate-500 flex flex-col w-1/2 justify-center items-center p-2 shadow-lg">
-                <div className="title self-start p-1">
-                    <h3 className="font-semibold text-slate-600">{data && data[0]?.title}</h3>
+                <div className="self-start p-1">
+                    <h2 className="font-semibold text-slate-600 text-lg">{data && data[1]?.title}</h2>
                 </div>
 
                 <div className="meme">
-                    <Meme source={data && data[0].url} />
+                    <Meme source={data && data[1].url} />
                 </div>
 
                 <div className="actionButtons">
                     <button className="border rounded p-2 border-slate-600 m-2 hover:bg-slate-600 hover:text-white"
                      onClick={()=> setFetchCategory('trending')}>Get Trending</button>
                     <button className="border rounded p-2 border-slate-600 m-2 hover:bg-slate-600 hover:text-white"
-                    onClick={()=> setFetchCategory('new')}
+                    onClick={()=> setFetchCategory('top')}
                     >Get New</button>
                 </div>
     </aside>
